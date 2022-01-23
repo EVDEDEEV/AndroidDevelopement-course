03e5e6ccf68184ff5a0365ba8af257aa569d4326
evdedeev <dedeeverkesh@gmail.com>
2022-01-23 11:30:06 +0300

    Copied code from youtube lesson

--- SecondWeek/app/src/main/java/com/example/secondweek/MainActivity.kt
+++ SecondWeek/app/src/main/java/com/example/secondweek/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.secondweek
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
+import android.view.ViewAnimationUtils
+import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.secondweek.*
 import com.example.secondweek.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import  com.google.android.material.navigation.NavigationBarView.OnItemSelectedListener
+import com.google.android.material.snackbar.Snackbar
+import android.view.View as View
 
 class MainActivity : AppCompatActivity(), View.OnClickListener, BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -23,8 +27,10 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, BottomNavigation
         setSupportActionBar(binding?.topAppBar)
         //^
         //байндинг для materialView
-        binding?.buttonMovie?.setOnClickListener(this)
+//        binding?.buttonMovie?.setOnClickListener(this)
         //^
+        binding?.fullIconButton?.setOnClickListener(this)
+
         supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
 
 //  Для показа иконки нажимаемого меню в Bottom navigation menu
@@ -49,70 +55,159 @@ class MainActivity : AppCompatActivity(), View.OnClickListener, BottomNavigation
     }
         //Для нижнего выезжающего окна с информацией
 
-        override fun onClick(view: View) {
+//        override fun onClick(view: View) {
+//
+//            Snackbar.make(view, R.string.snackbar, Snackbar.LENGTH_LONG)
+//
+//                .setAction(R.string.play) {
+//
+//                    binding?.result?.text = getString(R.string.pleasantViewing)
+//                }
+//
+//
+//                .setBackgroundTint(ContextCompat.getColor(this, R.color.black))
+//                .setTextColor(ContextCompat.getColor(this, R.color.white))
+//                .setActionTextColor(ContextCompat.getColor(this, R.color.orange))
+//                .show()
+//
+//        }
+////            override fun onClick(view : View) {
+////
+////            val details = Details()
+////
+////            val parameters = Bundle()
+////
+////            parameters.putString("nameMovie", binding?.nameMovie?.text?.toString())
+////            parameters.putString("actorsMovie", binding?.actorsMovie?.text?.toString())
+////
+////            details.arguments = parameters
+////
+////            details.show(supportFragmentManager, "details")
+////
+////
+////
+////        //^
+////
+////
+////
+////    binding?.topAppBar?.setOnMenuItemClickListener { menuItem:MenuItem ->
+////
+////        when(menuItem.itemId) {
+////
+////            R.id.favoritesItemTopNav -> {
+////                supportFragmentManager.beginTransaction().replace(R.id.content, Favorites()).commit()
+////
+////                true
+////
+////            }
+////
+////            R.id.settingsItemTopNav -> {
+////                supportFragmentManager.beginTransaction().replace(R.id.content, Settings()).commit()
+////
+////                true
+////
+////            }
+////
+////            else -> false
+////        }
+////
+////    }
+////}
+//
+//
+//
+//
+//
+//override fun onNavigationItemSelected(item: MenuItem): Boolean {
+//
+//    when(item.itemId) {
+//        R.id.homeItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
+//        R.id.shopItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Shop()).commit()
+//        R.id.deliveryItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Delivery()).commit()
+//        R.id.accountItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Account()).commit()
+//    }
+//
+//    return true
+//}
+//
+//override fun onCreateOptionsMenu(menu: Menu): Boolean {
+//
+//    val menuInflater = menuInflater
+//    menuInflater.inflate(R.menu.top_menu, menu)
+//
+//    return true
+//}
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//
+//        when (item.itemId) {
+//
+//            android.R.id.home -> {
+//
+//                val mainMenu = MainMenu()
+//                mainMenu.show(
+//                    supportFragmentManager,
+//                    "main_menu"
+//                )
+//
+//            }
+//
+//
+//        }
+//
+//        return super.onOptionsItemSelected(item)
+//
+//    }
+//}
 
-        val details = Details()
+    @SuppressLint("ResourceAsColor")
+    override fun onClick(view: View) {
 
-        val parameters = Bundle()
+        val builder = MaterialAlertDialogBuilder(this, R.style.MyDialogTheme)
 
-        parameters.putString("nameMovie", binding?.nameMovie?.text?.toString())
-        parameters.putString("longMovie", binding?.longMovie?.text?.toString())
-        parameters.putString("actorsMovie", binding?.actorsMovie?.text?.toString())
+            .setTitle(resources.getString(R.string.badBoys))
 
-        details.arguments = parameters
+            .setMessage(resources.getString(R.string.messageDialog))
 
-        details.show(supportFragmentManager, "details")
+            .setNeutralButton(resources.getString(R.string.close)) { dialog, which ->
 
-
-        //^
-
-
-
-    binding?.topAppBar?.setOnMenuItemClickListener { menuItem:MenuItem ->
-
-        when(menuItem.itemId) {
-
-            R.id.favoritesItemTopNav -> {
-                supportFragmentManager.beginTransaction().replace(R.id.content, Favorites()).commit()
-
-                true
+                loadNeutral()
 
             }
 
-            R.id.settingsItemTopNav -> {
-                supportFragmentManager.beginTransaction().replace(R.id.content, Settings()).commit()
+            .setNegativeButton(resources.getString(R.string.no)) { dialog, which ->
 
-                true
+                loadNegative()
 
             }
 
-            else -> false
-        }
+            .setPositiveButton(resources.getString(R.string.yes)) { dialog, which ->
+
+                loadPositive()
+
+            }
+
+            .show()
+
+        builder.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(ContextCompat.getColor (this, R.color.black))
+        builder.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(ContextCompat.getColor (this, R.color.black))
+        builder.getButton(AlertDialog.BUTTON_NEUTRAL).setTextColor(ContextCompat.getColor (this, R.color.black))
 
     }
-}
 
-override fun onNavigationItemSelected(item: MenuItem): Boolean {
 
-    when(item.itemId) {
-        R.id.homeItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
-        R.id.shopItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Shop()).commit()
-        R.id.deliveryItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Delivery()).commit()
-        R.id.accountItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Account()).commit()
+    private fun loadPositive() {
+        binding?.result?.text = getString(R.string.actionPositive)
     }
 
-    return true
+    private fun loadNeutral() {
+        binding?.result?.text = getString(R.string.actionNeutral)
+    }
+
+    private fun loadNegative() {
+        binding?.result?.text = getString(R.string.actionNegative)
+    }
+
 }
-
-override fun onCreateOptionsMenu(menu: Menu): Boolean {
-
-    val menuInflater = menuInflater
-    menuInflater.inflate(R.menu.top_menu, menu)
-
-    return true
-}
-}
-
 
 
 
