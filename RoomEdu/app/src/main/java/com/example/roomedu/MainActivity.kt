ee2efaf4da756cfa8083d1439d5c15552278cfc9
evdedeev <dedeeverkesh@gmail.com>
2022-01-28 16:00:47 +0300

    All objects initiated

--- RoomEdu/app/src/main/java/com/example/roomedu/MainActivity.kt
+++ RoomEdu/app/src/main/java/com/example/roomedu/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.roomedu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import com.example.roomedu.databinding.ActivityMainBinding
+import com.example.roomedu.tabs.TabCategories
+import com.example.roomedu.tabs.TabFilters
+import com.example.roomedu.tabs.TabPanel
+import com.example.roomedu.tabs.TabProducts
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+        supportFragmentManager.beginTransaction().replace(R.id.content, TabPanel()).commit()
+
+        binding?.bottomNav?.setOnNavigationItemSelectedListener(this)
+        binding?.bottomNav?.selectedItemId = R.id.panelItemBottomNav
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.panelItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, TabPanel()).commit()
+            R.id.catalogProductsItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, TabProducts()).commit()
+            R.id.catalogClothesItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, TabFilters()).commit()
+            R.id.catalogCategoriesItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, TabCategories()).commit()
+        }
+
+        return true
     }
 }
