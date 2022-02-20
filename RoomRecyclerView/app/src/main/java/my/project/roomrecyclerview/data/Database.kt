5ec80519e556ba262a90b07827381e8ed3d77542
evdedeev <dedeeverkesh@gmail.com>
2022-02-21 01:02:26 +0300

    1)Filled Database class 2)Fulled Category and Product Models (databases)

--- RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/data/Database.kt
+++ RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/data/Database.kt
@@ -1,4 +1,39 @@
 package my.project.roomrecyclerview.data
 
-class Database {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.synchronized
+import my.project.roomrecyclerview.models.CategoryModel
+import my.project.roomrecyclerview.models.ProductModel
+
+@Database(entities = [CategoryModel::class, ProductModel::class], version = 1)
+abstract class Database {
+    //Создаем переменные, которые наследуют интерфейсы управления БД
+    abstract val productDao: ProductDao
+    abstract val categoryDao: CategoryDao
+
+    //Стандартный код для Room ?
+    companion object {
+        @Volatile
+        private var INSTANCE: my.project.roomrecyclerview.data.Database? = null
+
+        @OptIn(InternalCoroutinesApi::class)
+        fun getInstance(context: Context): my.project.roomrecyclerview.data.Database {
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        my.project.roomrecyclerview.data.Database::class.java,
+
+                    ).build()
+                }
+                return instance
+            }
+
+
+        }
+    }
 }
--- RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/models/CategoryModel.kt
+++ RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/models/CategoryModel.kt
@@ -1,4 +1,15 @@
 package my.project.roomrecyclerview.models
 
-class CategoryModel {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "category_data_table")
+data class CategoryModel (
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "category_id")
+    var id : Int,
+
+    @ColumnInfo(name = "category_name")
+    var name : String
+        )
