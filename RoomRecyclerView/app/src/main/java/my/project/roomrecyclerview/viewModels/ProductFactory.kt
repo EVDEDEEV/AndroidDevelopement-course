88997d2d6a1eda407973c512fa876085da2b0268
evdedeev <dedeeverkesh@gmail.com>
2022-02-23 01:33:21 +0300

    Filled ProductFactory.kt

--- RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/viewModels/ProductFactory.kt
+++ RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/viewModels/ProductFactory.kt
@@ -1,4 +1,16 @@
 package my.project.roomrecyclerview.viewModels
 
-class ProductFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import my.project.roomrecyclerview.repositories.CategoryRepository
+import my.project.roomrecyclerview.repositories.ProductRepository
+
+class ProductFactory(private val productRepository: ProductRepository): ViewModelProvider.Factory {
+    override fun  <T: ViewModel> create (modelClass: Class<T>) : T {
+        if (modelClass.isAssignableFrom(ProductViewModel::class.java)) {
+            return ProductViewModel(productRepository) as T
+        }
+        throw IllegalArgumentException("Unknown View Model class")
+    }
+
 }
