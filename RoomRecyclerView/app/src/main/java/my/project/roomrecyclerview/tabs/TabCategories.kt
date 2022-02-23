b56d423b4c0540a2ce7872d1f444ba4a9852b219
evdedeev <dedeeverkesh@gmail.com>
2022-02-23 22:25:59 +0300

    Filled TabCategories.kt

--- RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/tabs/TabCategories.kt
+++ RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/tabs/TabCategories.kt
@@ -6,16 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import my.project.roomrecyclerview.R
+import my.project.roomrecyclerview.data.Database
 import my.project.roomrecyclerview.databinding.TabCategoriesBinding
 import my.project.roomrecyclerview.repositories.CategoryRepository
+import my.project.roomrecyclerview.viewModels.CategoryFactory
 import my.project.roomrecyclerview.viewModels.CategoryViewModel
 
 class TabCategories : Fragment() {
 
     private var binding: TabCategoriesBinding? = null
-    private var categoriesRepository: CategoryRepository? = null
-    private var categoriesViewModel: CategoryViewModel? = null
+    private var categoryRepository: CategoryRepository? = null
+    private var categoryViewModel: CategoryViewModel? = null
+    private var categoryFactory: CategoryFactory? = null
     private var categoryAdapter: CategoryAdapter? = null
 
     override fun onCreateView(
@@ -25,7 +32,29 @@ class TabCategories : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.tab_categories, container, false)
 
+        val categoriesDao = Database.getInstance((context as FragmentActivity).application).categoryDao
+        categoryRepository = CategoryRepository(categoriesDao)
+        categoryFactory = CategoryFactory(categoryRepository!!)
+        categoryViewModel = ViewModelProvider(this, categoryFactory!!).get(CategoryViewModel::class.java)
+
+        initRecyclerCategories()
+        displayCategories()
+
         return binding?.root
 
+    }
+
+    private fun initRecyclerCategories() {
+        binding?.recyclerCategories?.layoutManager = LinearLayoutManager(context)
+        categoryAdapter = CategoryAdapter()
+        binding?.recyclerCategories?.adapter = categoryAdapter
+
+    }
+
+    private fun displayCategories(){
+        categoryViewModel?.categories?.observe(viewLifecycleOwner, Observer {
+            categoryAdapter?.setList(it)
+            categoryAdapter?.notifyDataSetChanged()
+        })
     }
 }
