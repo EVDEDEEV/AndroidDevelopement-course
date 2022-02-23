6b9c1aac7317f271397e93a3bc784089c6092b04
evdedeev <dedeeverkesh@gmail.com>
2022-02-23 16:08:46 +0300

    1)Added code in activity_main.xml 2)Added bottom_menu.xml in menu resource file 3)Added color_tab.xml in color resource file 4)Added code for activity_main.xml logic in MainActivity.kt

--- RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/MainActivity.kt
+++ RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/MainActivity.kt
@@ -2,10 +2,51 @@ package my.project.roomrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import my.project.roomrecyclerview.databinding.ActivityMainBinding
+import my.project.roomrecyclerview.tabs.TabCategories
+import my.project.roomrecyclerview.tabs.TabFilters
+import my.project.roomrecyclerview.tabs.TabPanel
+import my.project.roomrecyclerview.tabs.TabProducts
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(R.id.content, TabPanel()).commit()
+
+        binding?.bottomNav?.setOnNavigationItemSelectedListener(this)
+        binding?.bottomNav?.selectedItemId = R.id.panelItemBottomNav
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.panelItemBottomNav -> supportFragmentManager.beginTransaction()
+                .replace(R.id.content, TabPanel()).commit()
+            R.id.catalogProductsItemBottomNav -> supportFragmentManager.beginTransaction()
+                .replace(R.id.content, TabProducts()).commit()
+            R.id.catalogClothesItemBottomNav -> supportFragmentManager.beginTransaction()
+                .replace(R.id.content, TabFilters()).commit()
+            R.id.catalogCategoriesItemBottomNav -> supportFragmentManager.beginTransaction()
+                .replace(R.id.content, TabCategories()).commit()
+        }
+
+        return true
     }
 }
+
+
+
+
+
+
