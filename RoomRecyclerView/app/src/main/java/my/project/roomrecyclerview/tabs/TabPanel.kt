3b4de20b81ca7cac5d561bee58f5ef3383960ebd
evdedeev <dedeeverkesh@gmail.com>
2022-02-24 00:26:20 +0300

    Filled TabPanel.kt

--- RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/tabs/TabPanel.kt
+++ RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/tabs/TabPanel.kt
@@ -5,56 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.CallbackRegistry
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModelProvider
 import my.project.roomrecyclerview.R
+import my.project.roomrecyclerview.data.Database
+import my.project.roomrecyclerview.databinding.TabPanelBinding
+import my.project.roomrecyclerview.repositories.CategoryRepository
+import my.project.roomrecyclerview.repositories.ProductRepository
+import my.project.roomrecyclerview.viewModels.CategoryFactory
+import my.project.roomrecyclerview.viewModels.CategoryViewModel
+import my.project.roomrecyclerview.viewModels.ProductFactory
+import my.project.roomrecyclerview.viewModels.ProductViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [TabPanel.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TabPanel : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var binding:TabPanelBinding? = null
+    private var categoryRepository: CategoryRepository? = null
+    private var categoryViewModel: CategoryViewModel? = null
+    private var categoryFactory: CategoryFactory? = null
+
+    private var productRepository: ProductRepository? = null
+    private var productViewModel: ProductViewModel? = null
+    private var productFactory: ProductFactory? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.tab_panel, container, false)
-    }
+        binding = DataBindingUtil.inflate(inflater, R.layout.tab_panel, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TabPanel.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TabPanel().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val categoryDao = Database.getInstance((context as FragmentActivity).application).categoryDao
+        categoryRepository = CategoryRepository(categoryDao)
+        categoryFactory = CategoryFactory(categoryRepository!!)
+        categoryViewModel = ViewModelProvider(this, categoryFactory!!).get(CategoryViewModel::class.java)
+
+        val productDao = Database.getInstance((context as FragmentActivity).application).productDao
+        productRepository = ProductRepository(productDao)
+        productFactory = ProductFactory(productRepository!!)
+        productViewModel = ViewModelProvider(this, productFactory!!).get(ProductViewModel::class.java)
+
+
+
+
+        return binding?.root
     }
 }
