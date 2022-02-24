087165a4644d5ddccdaafeee372973b11bef5bbe
evdedeev <dedeeverkesh@gmail.com>
2022-02-24 16:24:25 +0300

    Filled TabPanel.kt

--- RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/tabs/TabPanel.kt
+++ RoomRecyclerView/app/src/main/java/my/project/roomrecyclerview/tabs/TabPanel.kt
@@ -1,6 +1,7 @@
 package my.project.roomrecyclerview.tabs
 
 import android.os.Bundle
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -19,9 +20,11 @@ import my.project.roomrecyclerview.viewModels.CategoryViewModel
 import my.project.roomrecyclerview.viewModels.ProductFactory
 import my.project.roomrecyclerview.viewModels.ProductViewModel
 
-class TabPanel : Fragment() {
+class TabPanel : Fragment(), View.OnClickListener, View.OnKeyListener {
+    //создаем переменную байндинг для дальшнейгего создания датаБайндига фрагмента и xml шаблона
+    private var binding: TabPanelBinding? = null
 
-    private var binding:TabPanelBinding? = null
+    //создаем переменные которые наследуются от репозиториев, вью моделями и фабриками
     private var categoryRepository: CategoryRepository? = null
     private var categoryViewModel: CategoryViewModel? = null
     private var categoryFactory: CategoryFactory? = null
@@ -35,20 +38,107 @@ class TabPanel : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.tab_panel, container, false)
-
-        val categoryDao = Database.getInstance((context as FragmentActivity).application).categoryDao
+//        создаем методы для заполнения шаблона
+        val categoryDao =
+            Database.getInstance((context as FragmentActivity).application).categoryDao
         categoryRepository = CategoryRepository(categoryDao)
         categoryFactory = CategoryFactory(categoryRepository!!)
-        categoryViewModel = ViewModelProvider(this, categoryFactory!!).get(CategoryViewModel::class.java)
+        categoryViewModel =
+            ViewModelProvider(this, categoryFactory!!).get(CategoryViewModel::class.java)
 
         val productDao = Database.getInstance((context as FragmentActivity).application).productDao
         productRepository = ProductRepository(productDao)
         productFactory = ProductFactory(productRepository!!)
-        productViewModel = ViewModelProvider(this, productFactory!!).get(ProductViewModel::class.java)
+        productViewModel =
+            ViewModelProvider(this, productFactory!!).get(ProductViewModel::class.java)
 
+//Логика для прослушивания кнопок
+//Кнопки для добавления товаров
+        binding?.enterNameProduct?.setOnClickListener(this)
+        binding?.enterCategoryProduct?.setOnClickListener(this)
+        binding?.enterPriceProduct?.setOnClickListener(this)
 
+//Кнопка для добавления продукта
+        binding?.buttonAddProduct?.setOnClickListener(this)
 
+//Кнопки для добавления категорий
+        binding?.buttonAddCategoryClothes?.setOnClickListener(this)
+        binding?.buttonAddCategoryShoes?.setOnClickListener(this)
+        binding?.buttonAddCategoryAccessories?.setOnClickListener(this)
 
         return binding?.root
+    }
+
+//Логика для полей ввода с клавиатуры
+    override fun onKey(view: View, i: Int, keyEvent: KeyEvent): Boolean {
+        when (view.id) {
+
+            R.id.enterNameProduct -> {
+                if (keyEvent.action == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_ENTER) {
+                    binding?.resEnterNameProduct?.text = binding?.enterNameProduct?.text
+                    binding?.enterNameProduct?.setText("")
+                    return true
+                }
+            }
+
+            R.id.enterCategoryProduct -> {
+                if (keyEvent.action == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_ENTER) {
+                    binding?.resEnterCategoryProduct?.text = binding?.enterCategoryProduct?.text
+                    binding?.enterCategoryProduct?.setText("")
+                    return true
+                }
+            }
+
+            R.id.enterPriceProduct -> {
+                if (keyEvent.action == KeyEvent.ACTION_DOWN && i == KeyEvent.KEYCODE_ENTER) {
+                    binding?.resEnterPriceProduct?.text = binding?.enterPriceProduct?.text
+                    binding?.enterPriceProduct?.setText("")
+                    return true
+                }
+            }
+        }
+    return false
+    }
+
+
+
+
+//Логика обработки клика по кнопкам
+    override fun onClick(view: View) {
+        when (view.id) {
+
+            R.id.buttonAddCategoryClothes -> {
+
+                categoryViewModel?.startInsert(binding?.buttonAddCategoryClothes?.text?.toString()!!)
+            }
+
+            R.id.buttonAddCategoryShoes -> {
+
+                categoryViewModel?.startInsert(binding?.buttonAddCategoryShoes?.text?.toString()!!)
+            }
+            R.id.buttonAddCategoryAccessories -> {
+
+                categoryViewModel?.startInsert(binding?.buttonAddCategoryShoes?.text?.toString()!!)
+            }
+
+            R.id.buttonAddProduct -> {
+
+                productViewModel?.startInsert(binding?.resEnterNameProduct?.text?.toString()!!,
+                    binding?.resEnterCategoryProduct?.text?.toString()!!,
+                    binding?.resEnterPriceProduct?.text?.toString()!!)
+
+                clearResEnterProduct()
+            }
+
+
+        }
+
+
+    }
+
+    private fun clearResEnterProduct() {
+        binding?.resEnterNameProduct?.setText("")
+        binding?.resEnterCategoryProduct?.setText("")
+        binding?.resEnterPriceProduct?.setText("")
     }
 }
