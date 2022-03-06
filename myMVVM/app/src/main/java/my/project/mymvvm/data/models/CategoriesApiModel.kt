e7f8080a52024fd941663221f11098275f083258
evdedeev <dedeeverkesh@gmail.com>
2022-03-06 22:02:06 +0300

    Added and filled CategoriesApiModel.kt

---
+++ myMVVM/app/src/main/java/my/project/mymvvm/data/models/CategoriesApiModel.kt
@@ -0,0 +1,14 @@
+package my.project.mymvvm.data.models
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class CategoriesApiModel (
+    @SerializedName("id") @Expose
+    var id: Int? = null,
+    @SerializedName("name") @Expose
+    var name: String? = null,
+    @SerializedName("image") @Expose
+    var image: String? = null
+
+        )
