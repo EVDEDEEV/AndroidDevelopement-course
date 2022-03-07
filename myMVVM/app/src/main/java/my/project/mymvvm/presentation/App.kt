5394dd3797148ff0c4d6f0268e2126d5bb0c10e8
evdedeev <dedeeverkesh@gmail.com>
2022-03-07 22:42:58 +0300

    Added and filled App.kt (dependence injection - Koin)

---
+++ myMVVM/app/src/main/java/my/project/mymvvm/presentation/App.kt
@@ -0,0 +1,27 @@
+package my.project.mymvvm.presentation
+
+import android.app.Application
+import my.project.mymvvm.presentation.di.moduleCategories
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            // Koin Android logger
+            androidLogger()
+            //inject Android context
+            androidContext(this@App)
+
+            modules(moduleCategories)
+
+        }
+
+    }
+
+
+}
