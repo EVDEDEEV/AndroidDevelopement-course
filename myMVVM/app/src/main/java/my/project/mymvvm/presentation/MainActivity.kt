82978d83d3cb5a1d6b9056c263504465020b6474
evdedeev <dedeeverkesh@gmail.com>
2022-03-08 00:09:10 +0300

    Filled MainActivity.kt

--- myMVVM/app/src/main/java/my/project/mymvvm/presentation/MainActivity.kt
+++ myMVVM/app/src/main/java/my/project/mymvvm/presentation/MainActivity.kt
@@ -7,18 +7,34 @@ import my.project.mymvvm.R
 import my.project.mymvvm.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity :  AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private var binding:ActivityMainBinding? = null
     val categoriesViewModel: CategoriesViewModel by viewModel()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-            binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
+
+        binding?.bottomNav?.selectedItemId = R.id.homeItemBottomNav
+
+        binding?.bottomNav?.setOnItemSelectedListener { item ->
+
+            when(item.itemId) {
+                R.id.homeItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Home()).commit()
+                R.id.categoriesItemBottomNav -> supportFragmentManager.beginTransaction().replace(R.id.content, Categories()).commit()
+
+            }
+
+            return@setOnItemSelectedListener true
+        }
 
         categoriesViewModel.migration(this)
     }
+
 
 }
