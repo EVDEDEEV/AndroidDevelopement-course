734f1241e7f2a8d92cfef23d570c061c14d684f5
evdedeev <dedeeverkesh@gmail.com>
2022-03-07 23:36:49 +0300

    Added and filled App.kt (dependence injection - Koin)

--- myMVVM/app/src/main/java/my/project/mymvvm/presentation/MainActivity.kt
+++ myMVVM/app/src/main/java/my/project/mymvvm/presentation/MainActivity.kt
@@ -2,13 +2,23 @@ package my.project.mymvvm.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import my.project.mymvvm.R
+import my.project.mymvvm.databinding.ActivityMainBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity :  AppCompatActivity() {
 
+    private var binding:ActivityMainBinding? = null
+    val categoriesViewModel: CategoriesViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+            binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+        categoriesViewModel.migration(this)
     }
 
 }
