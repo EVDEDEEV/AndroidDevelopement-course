46818b25cc3265597ffb8622d0fa5162e0ef3f79
evdedeev <dedeeverkesh@gmail.com>
2022-03-07 19:00:20 +0300

    Added and filled CategoriesViewModel.kt

---
+++ myMVVM/app/src/main/java/my/project/mymvvm/presentation/CategoriesViewModel.kt
@@ -0,0 +1,21 @@
+package my.project.mymvvm.presentation
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import my.project.mymvvm.domain.useCase.CategoriesUseCase
+
+class CategoriesViewModel (private val categoriesUseCase: CategoriesUseCase):
+    ViewModel() {
+
+    val loadCategories = categoriesUseCase.loadCategories()
+
+
+    fun migration(context: Context) = viewModelScope.launch {
+        categoriesUseCase.startMigration(context)
+
+    }
+
+
+}
