956b3b9a69d0d885459d877f7b34ffdefebacd11
evdedeev <dedeeverkesh@gmail.com>
2022-03-10 16:57:29 +0300

    Start

---
+++ Cofee/app/src/main/java/my/project/cofee/presentation/App.kt
@@ -0,0 +1,22 @@
+package my.project.cofee.presentation
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            //Koin Android logger
+            androidLogger()
+            //inject Android context
+            androidContext(this@App)
+
+            modules()
+        }
+    }
+}
---
+++ Cofee/app/src/main/java/my/project/cofee/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package my.project.cofee.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import my.project.cofee.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
