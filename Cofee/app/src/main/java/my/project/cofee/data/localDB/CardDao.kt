ef4cda311e2bc9f6bd1b63e1faca06d829632be2
evdedeev <dedeeverkesh@gmail.com>
2022-03-12 19:46:12 +0300

    Added and filled CardDao.kt(interface for CaDB local DB)

--- Cofee/app/src/main/java/my/project/cofee/data/localDB/CardDao.kt
+++ Cofee/app/src/main/java/my/project/cofee/data/localDB/CardDao.kt
@@ -5,17 +5,18 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import my.project.cofee.data.models.CardModel
 import my.project.cofee.data.models.CoffeeModel
 
 @Dao
-interface CoffeeDao {
+interface CardDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(coffeeModel: CoffeeModel)
+    fun insert(cardModel: CardModel)
 
-    @Query("SELECT * FROM coffee_data_table")
-    fun loadCoffee(): LiveData<List<CoffeeModel>>
+    @Query("SELECT * FROM card_data_table")
+    fun loadCoffeeFromCard(): LiveData<List<CardModel>>
 
-    @Query("DELETE FROM coffee_data_table")
+    @Query("DELETE FROM card_data_table")
     fun clear()
 }
