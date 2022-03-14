9dab606e9791b679c46cd642b63150e76def5028
evdedeev <dedeeverkesh@gmail.com>
2022-03-14 18:26:48 +0300

    Added and filled Card layout(logic and binding)

--- Cofee/app/src/main/java/my/project/cofee/presentation/Card.kt
+++ Cofee/app/src/main/java/my/project/cofee/presentation/Card.kt
@@ -5,56 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import my.project.cofee.R
+import my.project.cofee.databinding.CardBinding
+import my.project.cofee.databinding.CardItemBinding
+import my.project.cofee.presentation.viewModels.CardViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Card.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Card : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var binding : CardBinding? = null
+    private var cardAdapter: CardAdapter? = null
+    private val cardViewModel: CardViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.card, container, false)
+
+        initRecyclerCard()
+        loadCoffeeFromCard()
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.card, container, false)
+        return binding?.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Card.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Card().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun initRecyclerCard() {
+
+        binding?.listCard?.layoutManager =
+            LinearLayoutManager(context)
+        cardAdapter =
+            CardAdapter ()
+        binding?.listCard?.adapter = cardAdapter
     }
+
+    private fun loadCoffeeFromCard() {
+
+        cardViewModel.loadCoffeeFromCard.observe(viewLifecycleOwner, Observer {
+            cardAdapter?.setList(it)
+            cardAdapter?.notifyDataSetChanged()
+        })
+
+
+    }
+
+
 }
+
+
+
+
