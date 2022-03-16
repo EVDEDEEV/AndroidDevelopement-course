85b9a45115994009c462ccde5d0a52f16a37ef13
evdedeev <dedeeverkesh@gmail.com>
2022-03-16 20:55:19 +0300

    Added @Update function updateProductToCard for less and more count buttons in CardCall.kt

--- Cofee/app/src/main/java/my/project/cofee/data/localDB/CardDao.kt
+++ Cofee/app/src/main/java/my/project/cofee/data/localDB/CardDao.kt
@@ -1,10 +1,7 @@
 package my.project.cofee.data.localDB
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import my.project.cofee.data.models.CardModel
 import my.project.cofee.data.models.CoffeeModel
 
@@ -20,6 +17,9 @@ interface CardDao {
     //Отслеживание нужного товара в корзине по id
     @Query("SELECT * FROM card_data_table WHERE card_idProduct = :idProduct")
     fun loadCoffeeToCardFromCardProduct(idProduct: String): LiveData<List<CardModel>>
+
+    @Update
+    suspend fun updateProductToCard(cardModel: CardModel)
 
     @Query("DELETE FROM card_data_table WHERE card_id = :idProductToCard")
     suspend fun deleteProductFromCard(idProductToCard: Int)
