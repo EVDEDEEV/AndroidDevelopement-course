48ff8cff7295dffc80d1d7cf38b315e43a1b5665
evdedeev <dedeeverkesh@gmail.com>
2022-03-16 18:57:14 +0300

    Changed entities name

--- Cofee/app/src/main/java/my/project/cofee/data/localDB/OrDB.kt
+++ Cofee/app/src/main/java/my/project/cofee/data/localDB/OrDB.kt
@@ -3,8 +3,9 @@ package my.project.cofee.data.localDB
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import my.project.cofee.data.models.CardModel
+import my.project.cofee.data.models.OrderLocalModel
 
-@Database(entities = [CardModel::class], version = 1)
+@Database(entities = [OrderLocalModel::class], version = 1)
 abstract class OrDB:RoomDatabase() {
     abstract val cardDao: CardDao
 }
