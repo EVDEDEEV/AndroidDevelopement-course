4425e177dcd5ee70adf853e66bd91a31cc63248a
evdedeev <dedeeverkesh@gmail.com>
2022-03-16 20:52:13 +0300

    Added order functions in App.kt

--- Cofee/app/src/main/java/my/project/cofee/presentation/App.kt
+++ Cofee/app/src/main/java/my/project/cofee/presentation/App.kt
@@ -3,6 +3,7 @@ package my.project.cofee.presentation
 import android.app.Application
 import my.project.cofee.presentation.di.card
 import my.project.cofee.presentation.di.coffee
+import my.project.cofee.presentation.di.order
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -18,7 +19,7 @@ class App : Application() {
             //inject Android context
             androidContext(this@App)
 
-            modules(coffee, card)
+            modules(coffee, card, order)
         }
     }
 }
